57ca6efd4630004ed3bd13e5c41e941c2688e5c3
bolar756 <bolarinwamartins79@gmail.com>
2023-12-22 14:03:32 +0100

    all for vercel

--- rhomart/build_files.sh
+++ rhomart/build_files.sh
@@ -1,7 +1,7 @@
 #usr/bin/bash
 echo starting file
-pip -r install requiremnts.txt
+python 3.9 -m pip -r install requiremnts.txt
 
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python 3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
