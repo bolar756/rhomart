c44ef5f91fcc74ff43c80b614d75a01e925c4dd7
bolar756 <bolarinwamartins79@gmail.com>
2023-12-22 13:41:54 +0100

    continue

--- rhomart/rome/models.py
+++ rhomart/rome/models.py
@@ -7,7 +7,7 @@ class Register(models.Model):
     category = models.CharField(default='category',max_length=20)
     amount=models.FloatField()
     age = models.IntegerField()
-    name = models.CharField(default=time.ctime(),max_length=20)
+    name = models.CharField(default=time.ctime(),max_length=255)
     gender=models.CharField(default='alien',max_length=20)
 
     def __repr__(self):
